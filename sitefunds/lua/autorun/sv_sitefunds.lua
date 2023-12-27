d83f0956a6be41b3e081562d7e3c88b7799a179b
Orion <145623338+Thrawnnn@users.noreply.github.com>
2023-12-27 10:49:28 -0600

    Update sv_sitefunds.lua

--- sitefunds/lua/autorun/sv_sitefunds.lua
+++ sitefunds/lua/autorun/sv_sitefunds.lua
@@ -2,12 +2,12 @@
 -- Copyright (c) 2023, Complexity Networks All Rights Reserved.
 
 local config = {}
-local fodeath = 10
-local breaches = 1
+local val2 = 10
+local val1 = 1 -- Place your own value here.
 
 config.DefaultFunds = 5000 -- Required config number
 config.Delay = 2 -- number in seconds of the delay between each fund addition
-config.Multiplier = fodeath / 5 + breaches
+config.Multiplier = val2 / 5 + val1
 config.FundIncrement = 20 - config.Multiplier -- Increment per second of the funds
 config.HudEnable = true
 config.showhud = 1 -- 1 is yes, anything else is no. Requires a restart to change.
