0df84d7a005cc23fd9abb6310427164aa381017d
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-01-29 11:33:04 -0600

    Remove config from sv_sitefunds

--- sitefunds/lua/autorun/sv_sitefunds.lua
+++ sitefunds/lua/autorun/sv_sitefunds.lua
@@ -1,19 +1,6 @@
 -- COPYRIGHT:
 -- Copyright (c) 2023, Complexity Networks All Rights Reserved.
-
 local config = {}
-local val2 = 10
-local val1 = 1 -- Place your own value here.
-
-config.DefaultFunds = 5000 -- Required config number
-config.Delay = 2 -- number in seconds of the delay between each fund addition
-config.Multiplier = val2 / 5 + val1
-config.FundIncrement = 20 - config.Multiplier -- Increment per second of the funds
-config.HudEnable = true
-config.showhud = 1 -- 1 is yes, anything else is no. Requires a restart to change.
-
-    
--- End configuration.
 
 --[[
 DONT TOUCH ANYTHING BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING.
