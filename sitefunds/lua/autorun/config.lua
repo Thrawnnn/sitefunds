b71a26e2d6c3e32b6b3d29a34b09a8493ed0f1d8
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-01-29 11:33:31 -0600

    Add configuration variables to config.lua

--- sitefunds/lua/autorun/config.lua
+++ sitefunds/lua/autorun/config.lua
@@ -1,1 +1,10 @@
+local config = {}
+local val2 = 10
+local val1 = 1 -- Place your own value here.
 
+config.DefaultFunds = 5000 -- Required config number
+config.Delay = 2 -- number in seconds of the delay between each fund addition
+config.Multiplier = val2 / 5 + val1
+config.FundIncrement = 20 - config.Multiplier -- Increment per second of the funds
+config.HudEnable = true
+config.showhud = 1 -- 1 is yes, anything else is no. Requires a restart to change.
