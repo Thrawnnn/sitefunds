8fc2cab0def101c5566f5506887c346ce775e483
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-02-16 23:52:10 -0600

    Delete sitefunds directory

--- sitefunds/lua/autorun/server/sv_hooks.lua
+++
@@ -1,4 +0,0 @@
-hook.Add( "PlayerDeath", "Reset", function( v, i, a )
-    hasClaimedWeps = false
-    hasClaimedHealth = false
-end ) -- resets the player's claimed status for each when they die so they can grab the earned stuff after every death
