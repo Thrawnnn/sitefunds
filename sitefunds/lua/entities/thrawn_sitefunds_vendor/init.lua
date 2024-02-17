2683826de7ded51240c48dec6b05e984e61ea39c
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-02-17 00:01:15 -0600

    Update init.lua

--- sitefunds/lua/entities/thrawn_sitefunds_vendor/init.lua
+++ sitefunds/lua/entities/thrawn_sitefunds_vendor/init.lua
@@ -24,22 +24,4 @@ function ENT:Use(ply,a)
     net.Start("OpenVendor")
 	net.Send(a)
 
---[[
-    if funding < 4999 then
-        return
-    elseif funding >= 5000 and funding <= 5999 then
-        ply:Give("plutonic_usp")
-        ply:Give("plutonic_mcx")
-        ply:SetHealth( 110 )
-        ply:SetArmor( 25 )
-        funding = funding - funding  -- Add setarmor and stuff too so there's more incentive, also change the goddamn weapon to a config.
-    elseif funding > 5999 then
-        ply:Give("plutonic_zoomar2")
-        ply:Give("plutonic_357")
-        ply:SetHealth( 125 )
-        ply:SetArmor( 50 )
-        funding = funding - funding 
-    end
-]]--
-
 end
